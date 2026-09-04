02de3b34f9f550130bbed4dfb7a910c7b1f183f4
Elson <2605775@sit.singaporetech.edu.sg>
2026-09-04 15:28:06 +0800

    Fourth activity

---
+++ Activity4.py
@@ -0,0 +1,13 @@
+username = input("Enter username: ")
+age = input ("Enter Age: ")
+category = input ("Enter Content Category: ")
+
+
+print("\nInstagram Profile")
+print("===================")
+print("Username: ",username)
+print("Age: ",age)
+print("Category:",category)
+
+#2a.the input() captures user's input by asking the user to enter
+#2b. the program is more dynamic  since it does not hard code the data for the user
