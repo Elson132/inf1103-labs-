0c81fbcf5c536bddf2b4dfce93abbbef1da5c6e5
Elson <2605775@sit.singaporetech.edu.sg>
2026-09-04 15:35:15 +0800

    Fifth Activity

--- hello.py
+++ hello.py
@@ -2,3 +2,7 @@ print("====================================")
 print("Welcome here")
 print("My first post!")
 print("====================================")
+
+#3a.Yes.The program display exactly as written
+#3b.The order is from top to bottom
+#3c.It appears in the terminal
